3b3a0eed1d7aeb3b5d7d12ad944bcef0923447ed
AnnaAvl <86964707+AnnaAvl@users.noreply.github.com>
2021-11-10 08:39:53 +0300

    commit #3

--- src/com/java/Main.java
+++ src/com/java/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello! How are you?");
         int a = 0;
+        for (int i = 0; i <= 10; i++) {
+            a += i;
+        }
     }
 }
